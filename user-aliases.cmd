0640671dc5a0632ebece1c22b36f19fa94b7523d
heguilong <hgleagle@sina.com>
2016-09-15 20:13:23 +0800

    add cmder config

---
+++ user-aliases.cmd
@@ -0,0 +1,22 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat %CMDER_ROOT%\config\.history
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+l=ls --show-control-chars
+la=ls -aF --show-control-chars
+ll=ls -alF --show-control-chars
+ls=ls --show-control-chars -F
+sshvps=ssh -p 22 heguilong@172.18.34.187
+sshvultr=ssh -p 22 root@45.76.105.22
+sshrpi=ssh -p 22 rxhf@192.168.195.230
